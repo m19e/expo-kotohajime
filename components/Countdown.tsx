84d1ab378d23615c0057ba3b1a4658c5960e5e9b
m19e <nexus.me7290@gmail.com>
2020-11-26 06:18:11 +0900

    Spacing buttons

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { StyleSheet, View } from "react-native";
 import { Button, Card } from "react-native-elements";
 import moment from "moment";
 
@@ -15,10 +16,24 @@ const secToMMSS = (sec: number): string => moment.utc(sec * 1000).format("mm:ss"
 const CountdownComponent = ({ leftSec, active, start, reset, stop }: CountdownProps): any => (
     <Card>
         <time style={{ fontSize: "160px" }}>{secToMMSS(leftSec)}</time>
-        <Button title="Start" onPress={start} disabled={active} />
-        <Button title="Stop" onPress={stop} disabled={!active} />
-        <Button title="Reset" onPress={reset} />
+        <View style={{ flex: 1, flexDirection: "row", justifyContent: "center", paddingBottom: 30 }}>
+            <Button title="Start" onPress={start} disabled={active} style={styles.button} />
+            <Button title="Stop" onPress={stop} disabled={!active} style={styles.button} />
+            <Button title="Reset" onPress={reset} style={styles.button} />
+        </View>
     </Card>
 );
+
+const styles = StyleSheet.create({
+    button: {
+        width: 100,
+        height: 30,
+        padding: 10,
+        backgroundColor: "lightgray",
+        alignItems: "center",
+        justifyContent: "center",
+        margin: 3,
+    },
+});
 
 export default CountdownComponent;
