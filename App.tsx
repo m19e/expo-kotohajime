e5571ca63df66a70011c3e4e135840bbc2ce5129
m19e <nexus.me7290@gmail.com>
2020-11-26 02:34:39 +0900

    Use state

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 
 const initialState = {
@@ -8,15 +8,24 @@ const initialState = {
 };
 
 export default function App() {
+    const [person, setPerson] = useState(initialState);
+
     const showAlert = () => {
         alert("display alert");
         console.log("print log on console");
     };
 
+    const changePerson = () => {
+        setPerson({
+            name: "Akiha",
+            age: 15,
+        });
+    };
+
     return (
         <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
-            <Button title="Click me" onPress={showAlert} />
+            <Text>Hello, {person.name}.</Text>
+            <Button title="Change name" onPress={changePerson} />
             <StatusBar style="auto" />
         </View>
     );
