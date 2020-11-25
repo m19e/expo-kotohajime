36bc2d840fa1ba9257c2827dc249f1a32d6a4fd4
m19e <nexus.me7290@gmail.com>
2020-11-26 06:17:11 +0900

    Add countdown container

---
+++ containers/Countdown.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import CountdownComponent from "../components/Countdown";
+
+const useCountdown = (limit: number): any => {
+    const [leftSec, setLeftSec] = useState(limit);
+    const [timerObj, setTimerObj]: any = useState("");
+    const [active, setActive] = useState(false);
+
+    const setCountdown = () => {
+        if (!active) {
+            setTimerObj(
+                setInterval(() => {
+                    setLeftSec((prev: number) => prev - 1);
+                }, 1000)
+            );
+        }
+    };
+
+    const afterTimeup = (left: number) => {
+        if (left <= 0) {
+            alert("Finished");
+            setLeftSec(() => limit);
+        }
+    };
+
+    const reset = () => {
+        if (true || window.confirm("Reset Timer?")) {
+            setLeftSec(limit);
+        }
+    };
+
+    const stop = () => {
+        setActive(false);
+        clearInterval(timerObj);
+    };
+
+    const start = () => {
+        if (!active) {
+            clearInterval(timerObj);
+            setCountdown();
+            setActive(true);
+        }
+    };
+
+    useEffect(() => {
+        setCountdown();
+        setActive(true);
+    }, []);
+
+    useEffect(() => {
+        afterTimeup(leftSec);
+    }, [leftSec]);
+
+    return [
+        [leftSec, active],
+        [reset, stop, start],
+    ];
+};
+
+const CountdownContainer = () => {
+    const TIMER = 60 * 25;
+    const [[leftSec, active], [reset, stop, start]] = useCountdown(TIMER);
+
+    return <CountdownComponent leftSec={leftSec} active={active} reset={reset} stop={stop} start={start} />;
+};
+
+export default CountdownContainer;
