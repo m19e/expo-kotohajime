75d267f302ecebcd9c70656e8b06a985ff928bcf
m19e <nexus.me7290@gmail.com>
2020-11-26 21:35:14 +0900

    Use expo-av for alarm

--- libs/useAudio.ts
+++ libs/useAudio.ts
@@ -1,29 +1,44 @@
 import { useState, useEffect } from "react";
+import { Audio } from "expo-av";
 
-const useAudio = (filepath: string): [playing: boolean, currentTime: number, play: () => any, pause: () => any, jump: (sec: number) => any] => {
-    const [audio] = useState(new Audio(filepath));
+const useAudio = (filepath: string): [play: () => any, pause: () => any] => {
+    const [audio] = useState(new Audio.Sound());
     const [, _forceUpdate] = useState(false);
     const forceUpdate = () => _forceUpdate((prev) => !prev);
 
-    useEffect(() => {
-        audio.play();
-        audio.addEventListener("play", forceUpdate);
-        audio.addEventListener("pause", forceUpdate);
-        audio.addEventListener("ended", forceUpdate);
-        audio.addEventListener("timeupdate", forceUpdate);
-        return () => {
-            audio.removeEventListener("play", forceUpdate);
-            audio.removeEventListener("pause", forceUpdate);
-            audio.removeEventListener("ended", forceUpdate);
-            audio.addEventListener("timeupdate", forceUpdate);
-        };
-    }, []);
+    (async () => {
+        useEffect(() => {
+            (async () => {
+                await audio.loadAsync(require("../assets/alarm.mp3"));
+                await audio.playAsync();
+            })();
 
-    const play = () => audio.play();
-    const pause = () => audio.pause();
-    const jump = (sec: number) => (audio.currentTime += sec);
+            // audio.play();
+            // audio.addEventListener("play", forceUpdate);
+            // audio.addEventListener("pause", forceUpdate);
+            // audio.addEventListener("ended", forceUpdate);
+            // audio.addEventListener("timeupdate", forceUpdate);
+            return () => {
+                (async () => {
+                    await audio.unloadAsync();
+                })();
+                // audio.removeEventListener("play", forceUpdate);
+                // audio.removeEventListener("pause", forceUpdate);
+                // audio.removeEventListener("ended", forceUpdate);
+                // audio.addEventListener("timeupdate", forceUpdate);
+            };
+        }, []);
+    })();
 
-    return [!audio.paused, audio.currentTime, play, pause, jump];
+    async function play() {
+        await audio.playAsync();
+    }
+    async function pause() {
+        await audio.pauseAsync();
+    }
+    // const jump = (sec: number) => (audio.currentTime += sec);
+
+    return [play, pause];
 };
 
 export default useAudio;
